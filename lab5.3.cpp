67366daf5a5d33cfcf3999c89717514e4eada17f
sofiia-pavl <72608224+sofiia-pavl@users.noreply.github.com>
2020-11-06 18:10:39 +0200

    Add files via upload

---
+++ lab5.3.cpp
@@ -0,0 +1,51 @@
+#include <iostream> 
+#include <cmath> 
+
+using namespace std;
+
+double f(const double x);
+
+int main() 
+{
+    double yp, yk, z;     
+    int n;
+
+    cout << "yp = "; cin >> yp;     
+    cout << "yk = "; cin >> yk;     
+    cout << "n  = "; cin >> n;
+
+    double dy = (yk - yp) / n;
+
+    double y = yp;
+
+    while (y <= yk) 
+    {
+        z = f(1 + y * y) + pow(f(f(1) + pow(f(y*y), 2)),2);         
+        cout << y << "  " << z << endl;         
+        y += dy;
+    }
+
+    return 0;
+}
+
+double f(const double x) 
+{
+    if (abs(x) >= 1)         
+        return (cos(x / 2.)/1 + sin(x)*sin(x));     
+    else 
+    {
+        double S = 0;         
+        int i = 0;        
+        double a = 1;        
+        S = a;        
+        do
+        {
+            i++;            
+            double R = x*x*x/((2.*i - 1)*2.*i);            
+            a *= R;            
+            S += a; 
+        }while (i < 6);        
+        return S;
+    }
+}
+
