05472228ea132c4045873fd129f5489f4ffd5a2e
sofiia-pavl <72608224+sofiia-pavl@users.noreply.github.com>
2020-11-07 18:44:24 +0200

    Add files via upload

---
+++ UnitTest lab51.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab51/lab5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest2
+{
+	TEST_CLASS(UnitTest2)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = h(9, 4);
+			Assert::AreEqual(t, 65);
+		}
+	};
+}
